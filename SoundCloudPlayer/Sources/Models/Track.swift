b9852d972b1d73f16c397d536b43c0ce6b3514b1
Dmitry Fomin <difomiry@gmail.com>
2019-01-25 20:07:46 +0000

    Added `Track` model

---
+++ SoundCloudPlayer/Sources/Models/Track.swift
@@ -0,0 +1,13 @@
+
+import Foundation
+
+struct Track: Codable {
+
+  let id: Int
+  let title: String
+  let genre: String
+  let duration: Int
+  let streamable: Bool
+  let downloadable: Bool
+
+}
