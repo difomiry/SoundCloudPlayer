82a4b53289ea7d2fc790a1954f52bf25596b4769
Dmitry Fomin <difomiry@gmail.com>
2019-01-29 05:07:59 +0000

    Added `User` model

--- SoundCloudPlayer/Sources/Models/Track.swift
+++ SoundCloudPlayer/Sources/Models/Track.swift
@@ -5,7 +5,22 @@ struct Track: Codable {
   let title: String
   let genre: String?
   let duration: Int
+  let artwork: String?
   let streamable: Bool?
   let downloadable: Bool?
+  let user: User
+
+  enum CodingKeys: String, CodingKey {
+
+    case id
+    case title
+    case genre
+    case duration
+    case artwork = "artwork_url"
+    case streamable
+    case downloadable
+    case user
+
+  }
 
 }
---
+++ SoundCloudPlayer/Sources/Models/User.swift
@@ -0,0 +1,16 @@
+
+struct User: Codable {
+
+  let id: Int
+  let avatar: String
+  let username: String
+
+  enum CodingKeys: String, CodingKey {
+
+    case id
+    case avatar = "avatar_url"
+    case username
+
+  }
+
+}
